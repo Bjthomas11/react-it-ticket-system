8592d5513fbe864b3682b07364333297d8a98332
Brian Thomas <brianthomas@Brians-Air.attlocal.net>
2022-01-31 12:09:48 -0600

    updated express setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Server started");
+const express = require('express')
+const dotenv = require("dotenv").config()
+
+const app = express();
+const PORT = process.env.PORT || 7000
+
+// routes
+app.get("/", (req,res) => {
+   res.status(200)
+})
+
+app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`))
