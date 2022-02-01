78d2cdface1349795c19d0f9f1f3c0c9d8ce8df1
Brian Thomas <brianthomas571@gmail.com>
2022-01-31 20:55:57 -0600

    finished home/login/register UI

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import { Fragment } from "react";
+import { FaQuestionCircle, FaTicketAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <div>
-    <h1>Home</h1>
-  </div>;
+  return (
+    <Fragment>
+      <section className="heading">
+        <h1>Need Help?</h1>
+        <p>Please select an option below</p>
+      </section>
+      <Link to="new-ticket" className="btn btn-reverse btn-block">
+        <FaQuestionCircle /> Create New Ticket
+      </Link>
+      <Link to="tickets" className="btn btn-block">
+        <FaTicketAlt /> View Tickets
+      </Link>
+    </Fragment>
+  );
 };
 
 export default Home;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,66 @@
+import { Fragment, useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+import { toast } from "react-toastify";
+
 const Login = () => {
-    return <div>
-        <h1>Login</h1>
-    </div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
   };
-  
-  export default Login;
-  
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Fragment>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please Login for Support</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              value={email}
+              name="email"
+              onChange={onChange}
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              name="password"
+              onChange={onChange}
+              placeholder="Enter password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </Fragment>
+  );
+};
+
+export default Login;
